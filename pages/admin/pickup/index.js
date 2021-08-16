0bc1c2d4d0b8a34d80fe1b1961e4e3d8176fde12
Wijayaac <wijayaac25@gmail.com>
2021-08-16 15:16:21 +0800

    🐛 timezone back to local

--- pages/admin/pickup/index.js
+++ pages/admin/pickup/index.js
@@ -272,13 +272,11 @@ export default function Pickup(props) {
                           <td>{item.name}</td>
                           <td>
                             {moment(item.start_at)
-                              .utcOffset(-480)
+                              .local(true)
                               .format("DD MMMM ,HH:mm")}
                           </td>
                           <td>
-                            {moment(item.end_at)
-                              .utcOffset(-480)
-                              .format("HH:mm")}
+                            {moment(item.end_at).local(true).format("HH:mm")}
                           </td>
                           <td>{item.route}</td>
                           <td>{item.ready === false ? "Pending" : "Ready"}</td>
